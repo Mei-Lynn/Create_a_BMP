683a793b458d9789ac24857956912718bd95e9a4
Mei Lynn <jorge.mezquiriz@live.u-tad.com>
2025-10-14 12:27:43 +0200

    basic file creator

--- src/generator/test.java
+++ src/generator/test.java
@@ -1,5 +1,19 @@
+package generator;
+
+import java.io.File;
+import java.io.IOException;
+
 public class test {
     public static void main(String[] args) {
-        
+        try {
+            File source = new File("img.bmp");
+            
+            if (!source.exists()) {
+                source.createNewFile();
+            }
+            
+            
+        } catch (IOException ex) {
+        }
     }
 }
