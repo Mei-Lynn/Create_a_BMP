9109680f5dcce4b1c633e91bec3288b43c3ce4c2
Mei Lynn <jorge.mezquiriz@live.u-tad.com>
2025-10-19 18:39:37 +0200

    Ajustes a nombres y progreso con el bmpHeader

--- src/generator/test.java
+++ src/generator/test.java
@@ -2,11 +2,13 @@ package generator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class test {
     /**
-     * Metodo necesario para crear little endians, es el formato estandar para valores numericos en bmpsç
-     * Java usa por defecto big endian, así que hay que leer desde el final
+     * Metodo necesario para crear little endians, es el formato estandar para valores numericos en bmps
+     * Java usa por defecto big endian, así que hay que leer desde el final para escribirlo en LE.
+     * Se usa ints en caso de que ocupe 4 bits
      * @param value
      * @return
      */
@@ -18,6 +20,20 @@ public class test {
             (byte) ((value >> 24) & 0xFF)  
         };
     }
+
+    /**
+     * Metodo necesario para crear little endians, es el formato estandar para valores numericos en bmps
+     * Java usa por defecto big endian, así que hay que leer desde el final para esribirlo en LE.
+     * Se usa shorts en caso de que ocupe 2 bits
+     * @param value
+     * @return
+     */
+    private static byte[] shortToLittleEndianBytes(short value) {
+    return new byte[] {
+        (byte) (value & 0xFF),         // Byte menos significativo
+        (byte) ((value >> 8) & 0xFF)   // Byte más significativo
+    };
+}
 
     /**
      * Genera el encabezado del archivo, 14 bytes con informacion sobre el propio archivo
@@ -37,7 +53,7 @@ public class test {
         rt[3] = bytesTamaño[1];
         rt[4] = bytesTamaño[2];
         rt[5] = bytesTamaño[3];
-        //Estoy evitando usar bucles deliberadamente para mantener una claridad visual de que se guarda en cada byte
+        //NOTA: Estoy evitando usar bucles deliberadamente para mantener una claridad visual de que se guarda en cada byte
 
         //Bytes 7-8-9-10, espacios reservados, deben ser 0
         rt[6] = 0;
@@ -56,31 +72,68 @@ public class test {
         return rt;
     }
     
-    private static byte[] BMPHeader () {
+    private static byte[] BMPHeader (int anchura, int altura) {
         byte[] rt = new byte[40];
+
+        //15-16-17-18, tamaño del encabezado de la info del bmp, 40 en little endian
+        byte[] bmpHeaderSize = intToLittleEndianBytes(40);
+        rt[0] = bmpHeaderSize[0];
+        rt[1] = bmpHeaderSize[1];
+        rt[2] = bmpHeaderSize[2];
+        rt[3] = bmpHeaderSize[3];
+
+        //18-19-20-21, anchura de la imagen
+        byte[] anchuraLE = intToLittleEndianBytes(anchura);
+        rt[4] = anchuraLE[0];
+        rt[5] = anchuraLE[1];
+        rt[6] = anchuraLE[2];
+        rt[7] = anchuraLE[3];
+
+        //22-23-24-25, altura de la imagen
+        byte[] alturaLE = intToLittleEndianBytes(altura);
+        rt[8] = alturaLE[0];
+        rt[9] = alturaLE[1];
+        rt[10] = alturaLE[2];
+        rt[11] = alturaLE[3];
+
+        //26-27, numero de planos, constante 1.
+        byte[] planos = shortToLittleEndianBytes((short) 1);
+        rt[12] = planos[0];
+        rt[13] = planos[1];
+
+        //28-29, tamaño de los puntos, es 
+
+        return rt;
     }
 
 
     public static void main(String[] args) {
-        try {
-            File source = new File("img.bmp");
+        try (Scanner sc = new Scanner(System.in)) { //Convenientemente declaramos el scanner con el try necesario para manejar files
             
-            if (!source.exists()) {
-                source.createNewFile();
+            //Hay que pedir el nombre del archivo
+            System.out.print("Introduce el nombre de la imagen: ");
+            String filename = sc.nextLine();
+
+            File img = new File(filename+".bmp");
+            
+            if (!img.exists()) {
+                img.createNewFile();
             }
 
             //Datos a pedir por el usuario
             int altura = 100;
             int anchura = 100;
-            //Constante, cuantos bytes usamos por pixel y cuanto ocupa el encabezado de un bmp
-            int bytesPorPixel = 24;
-            int bytesPorHeader = 54;
 
-            int tamaño = bytesPorHeader + altura * anchura * bytesPorPixel;
+            //Constante, cuantos bytes usamos por pixel y cuanto ocupa el encabezado de un bmp
+            int bitsPorPixel = 24; //8 por cada color
+            int bitsDelHeader = 54; //La cantidad de bits que vamos a ocupar para hacer el encabezado completo.
+
+            //Variables calculadas
+            int tamaño = bitsDelHeader + altura * anchura * bitsPorPixel;
             
             //Inicio de la construcción
             byte[] fileHeader = FileHeader(tamaño);
-
+            byte[] bmpHeader = BMPHeader(anchura, altura);
             
         } catch (IOException ex) {
         }
